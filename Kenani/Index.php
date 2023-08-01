581d4b37e3bdc76e22a255470411e7a5849fc69d
Qendrim <qendrim045@gmail.com>
2023-08-01 22:18:11 +0200

    Basic setup

---
+++ Kenani/Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="css" href="./Css.css">
+</head>
+<body>
+	<div>
+		<p class="p1">Hello World</p>
+	</div>
+
+	<!-- Shearch Box qe e ka me shkru te dhanat e subdomainit 
+
+psh: google.com (search) 
+
+incinailzohet me python brute force e cila ben kerkime online te subdomainve te google.com 
+//nepermjet listes e cila e ka me larte ose kudo qofte sipas dizajnit te frontit, me pas edhe ni uplouad .txt
+
+dmt lista e subdomainve potencial 
+-->
+
+</body>
+</html>
+
+
